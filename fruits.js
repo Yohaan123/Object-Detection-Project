10d5c0231d4156847be5635ed96fd65d5dbb32e6
Yohaan <80580077+Yohaan123@users.noreply.github.com>
2021-10-13 10:00:47 +0530

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,27 @@
+status =  "";
+
+function preload(){
+    img = loadImage("fruits.jpg");
+}
+
+function setup(){
+    canvas = createCanvas();
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML = "status : Detecting Objects"
+}
+
+function modelLoaded(){
+    console.log("model Loaded");
+    status = true;
+
+}
+
+function gotResults(error , result){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(result);
+    }
+}
